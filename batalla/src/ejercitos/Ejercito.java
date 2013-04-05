11b792c8af6d5f2ebdd02b4f0c910bce7cc00881
Victor <vicdejuan@Vicdejuan.(none)>
2013-04-05 17:34:43 +0200

    mas cosillas

---
+++ batalla/src/ejercitos/Ejercito.java
@@ -0,0 +1,5 @@
+package ejercitos;
+
+public class Ejercito {
+	//TODO:
+}
---
+++ batalla/src/ejercitos/Tropa.java
@@ -0,0 +1,56 @@
+package ejercitos;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
+import criaturas.Criatura;
+import criaturas.TipoEjercito;
+import factories.CriaturaFactoria;
+import factories.ElfoFactoria;
+import factories.EnanoFactoria;
+import factories.HumanoFactoria;
+
+public class Tropa {
+
+	TipoEjercito tipo;
+	int numGuerreros;
+	ArrayList<Criatura> guerreros;
+
+	public Tropa(CriaturaFactoria factoria, int numeroGuerreros) {
+		numGuerreros = numeroGuerreros;
+		guerreros = new ArrayList<Criatura>();
+		int i = 0;
+		for (i = 0; i < numeroGuerreros; ++i) {
+			// TODO: donde se guardan...? ALguna cosa mejor que arrayList
+			guerreros.add(factoria.crearCriatura());
+		}
+		if (factoria.getClass() == ElfoFactoria.class
+				|| factoria.getClass() == EnanoFactoria.class
+				|| factoria.getClass() == HumanoFactoria.class)
+			tipo = TipoEjercito.Libres;
+		else 
+			tipo = TipoEjercito.Oscuras;
+	}
+
+	public boolean estaAniquilada() {
+		return (numGuerreros == 0);
+	}
+
+	public void atacar(Tropa oponente) {
+		// TODO: de momento atacamos a un aleatorio
+		for (Criatura aux : guerreros)
+			aux.atacar(oponente.getCriatura());
+	}
+
+	public Criatura getCriatura() {
+		Random r = new Random(Calendar.getInstance().getTimeInMillis());
+		return this.guerreros.get(r.nextInt(guerreros.size()));
+
+	}
+
+	public void aplicarHeridas() {
+		for (Criatura aux : this.guerreros)
+			aux.aplicarHeridas();
+	}
+}
